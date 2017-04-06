a22293dc3edc10a9b2db6a890fbd343e97ce5c3c
karisma70 <karisma70kr@gmail.com>
2017-04-06 17:16:40 +0900

    Chapter14 폴더 생성    doc_add.js   : 레코드 3개 추가 샘플 코드

---
+++ Chapter14/doc_add.js
@@ -0,0 +1,33 @@
+/**
+ * Created by david on 2017-04-06.
+ */
+var MongoClient = require('mongodb').MongoClient;
+
+function addObject( collection, object ){
+    collection.insert( object, function(err, result ){
+       if(err){
+           console.log("collection.insert Error!!!  : " + object );
+           return;
+       }
+
+       console.log("Inserted : " + result );
+    });
+}
+
+// mongodb://test@localhost:27017
+MongoClient.connect( "mongodb://localhost:27017", function(err, db){
+    var myDB = db.db("astro");      // astro db 생성
+    myDB.dropCollection("nebulae");
+    myDB.createCollection("nebulae", function(err, ins_obj ){
+        addObject( ins_obj, { ngc: "NGC 7293", name : "Helix", type: "platetary", location: "Aquila"} );
+        addObject( ins_obj, { ngc: "NGC 6543", name : "Cat's Eye", type: "platetary", location: "Draco"} );
+        addObject( ins_obj, { ngc: "NGC 1592", name : "Crab", type: "supernova", location: "Taurus"} );
+
+        console.log("success !!  insert of 3 records");
+    })
+
+    setTimeout( function(){
+        db.close();
+        console.log(" db.close() <-----")
+    }, 3000 );
+} );
