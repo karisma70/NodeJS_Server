7340bbbf52f1c5fedabd7e33b7e083e5cd91e81b
karisma70 <karisma70kr@gmail.com>
2017-04-06 15:05:28 +0900

    db_create_list_delete.js       1. mongoDB의 db 생성과 collection 생성       2. drop db를 통한 db 삭제
    MongoClient_sample.js
          1. mongodb 드라이버 1.4 기준 샘플코드 수정

--- Chapter13/MongoClient_sample.js
+++ Chapter13/MongoClient_sample.js
@@ -1,10 +1,25 @@
 
+/*
 var MongoClient = require('mongodb').MongoClient,
     test = require('assert');
 // Connection url
-var url = 'mongodb://localhost:27017/test';
+// var url = 'mongodb://localhost:27017/test';
+var url = 'mongodb://test@localhost:27017';
 // Connect using MongoClient
-MongoClient.connect(url, function(err, db) {
+MongoClient.connect(url, {
+    db: { w:1, native_parser: false },
+    server: {
+        poolSize: 5,
+        socketOptions: { connectTimeoutMS: 500 },
+        auto_reconnect: true
+    },
+    replSet: {},
+    mongos: {}
+},
+    function(err, db) {
+    if( err ){
+        console.log("Connection Failed via connection String.");
+    }
     // Use the admin database for the operation
     var adminDb = db.admin();
     // List all the available databases
@@ -14,3 +29,39 @@ MongoClient.connect(url, function(err, db) {
         db.close();
     });
 });
+    */
+
+var MongoClient = require('mongodb').MongoClient;
+var test = require('assert');
+
+var url = 'mongodb://test@localhost:27017';
+
+MongoClient.connect( url ,{
+        db: { w:1, native_parser: false },
+        server: {
+            poolSize: 5,
+            socketOptions: { connectTimeoutMS: 500 },
+            auto_reconnect: true
+        },
+        replSet: {},
+        mongos: {}
+    },
+    function(err, db ){
+        if(err){
+            console.log("Connection Failed via connection String.");
+        }else{
+            console.log("Connected via connectin string.");
+
+            var adminDb = db.admin();
+
+            /*
+            // List all the available databases
+            adminDb.listDatabases(function(err, dbs) {
+                test.equal(null, err);
+                test.ok(dbs.databases.length > 0);
+                db.close();
+            });
+            */
+        }
+
+    } );
---
+++ Chapter13/db_create_list_delete.js
@@ -0,0 +1,56 @@
+/**
+ * Created by david on 2017-04-06.
+ */
+var MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect( "mongodb://localhost/", function(err, db){
+    if( err ){
+        console.log("Faile to connect localhost");
+        return;
+    }
+    var adminDB = db.admin();
+    adminDB.listDatabases( function(err, databases ){
+        console.log(" Before Add Data List: ");
+        console.log( databases );
+    });
+
+    var newDB = db.db("newDB"); // Create a new Db instance sharing the current socket connections.
+
+    newDB.createCollection( "newCollection", function(err, collection){     //  Collections 생성
+       if( err ) {
+           console.log("Call newCollection Fail!!");
+           return;
+       }
+        console.log("New Database and Collection Created.");
+        adminDB.listDatabases( function(err, databases){
+           console.log( "After add database List: ");
+           console.log( databases );
+           db.db("newDB").dropDatabase(function(err, results){
+               if( err ){
+                   console.log("Call newDB.dropDatabase Fail!!");
+                   return;
+               }
+               if( results == true )
+                    console.log("Database dropped!!");
+               else
+                    console.log( "Fail drop of Database !!!" );
+
+               setTimeout( function(){
+                  adminDB.listDatabases( function(err, results ){
+                      var found = false;
+                      for( var i = 0; i < results.databases.length; i++ ){
+                          if( results.databases[i].name == "newDB" )
+                              found = true;
+                      }
+                      if( !found ){
+                          console.log("After Delete Database List: " );
+                          console.log( results );
+                      }
+                      db.close();
+                  }) ;
+               }, 15000 );
+           }) ;
+        });
+    });
+
+} );
