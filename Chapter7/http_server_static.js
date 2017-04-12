ba1082129e322c545317a0f37597a956eeff2c33
David Lee <karisma70kr@gmail.com>
2017-04-12 17:05:35 +0900

    http_server_static.js 소스 수정과 Server가 전송할 수 있는 html 폴더의 샘플 파일 업로드

--- Chapter7/http_server_static.js
+++ Chapter7/http_server_static.js
@@ -7,26 +7,6 @@ var url = require('url');
 
 var ROOT_DIR = "html/";
 
-/*
-http.createServer( function(req, res){
-    var urlObj = url.parse( req.url, true, false );     // url을 파싱
-    fs.readFile(ROOT_DIR + urlObj.pathname, function(err, data){
-        if( err ){
-            console.log( JSON.stringify(err));
-            res.writeHead(404);
-            res.end(JSON.stringify(err));
-            return;
-        }
-        else {
-            console.log("Success!! " + (ROOT_DIR + urlObj.pathname) + " is sending OK!!" );
-            res.writeHead(200);
-            res.end(data);
-        }
-    });
-} ).listen(8081);       // 8081 포트를 열고 대기한다.
-*/
-
-
 var callbackServer = function(request, response){
     var urlObj = url.parse( request.url, true, false );     // url을 파싱
     fs.readFile(ROOT_DIR + urlObj.pathname, function(err, data){
