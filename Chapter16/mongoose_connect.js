dee9f443c626db109dc8aa8e2bbbb35a64140833
David Lee <karisma70kr@gmail.com>
2017-04-13 16:11:18 +0900

    mongoose를 사용한 query 사용하기

--- Chapter16/mongoose_connect.js
+++ Chapter16/mongoose_connect.js
@@ -2,13 +2,14 @@
  * Created by Administrator on 2017-04-13.
  */
 var mongoose = require('mongoose');
-mongoose.connect("mongodb://13.124.86.217:27017/words");
+mongoose.connect("mongodb://13.124.86.217/words");
+
+console.log("Trying connect mongodb.... ");
+
 mongoose.connection.on( 'open', function(){
     console.log(mongoose.connection.collection);
-    mongoose.connection.on('open', function(){
-       mongoose.connection.db.collectionNames( function(err, names){
+    mongoose.connection.db.collectionNames( function(err, names){
            console.log( names );
            mongoose.disconnect();
        }) ;
-    });
 } );
