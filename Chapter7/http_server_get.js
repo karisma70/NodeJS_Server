18f4c1aa6d4de567cca9f206c3c0863a76c2dae4
David Lee <karisma70kr@gmail.com>
2017-05-19 08:02:30 +0900

    동일 IP주소에서 XMLHttpRequest가 동작할 수 있도록 소스 수정

--- Chapter7/http_server_get.js
+++ Chapter7/http_server_get.js
@@ -3,20 +3,29 @@
  */
 
 var http = require('http');
+var url = require('url');
+
 var messages = [
         'Hello World',
         'From a basic Node.js server',
         'Take Luck'
     ];
 
-http.createServer( function (req, res){
-    res.setHeader("Content-Type", "text/html");
-    res.writeHead( 200 );
-    res.write('<html>\n<head>\n<title>Simple \n</title>\n</head>\n');
-    res.write('<body>\n');
+http.createServer( function (request, response){
+
+    var urlObj = url.parse( request.url, true, false );     // url을 파싱
+    console.log( "url : " + urlObj.pathname );
+
+    //res.setHeader("Content-Type", "text/html");
+    response.writeHead( 200, {
+        "Content-Type" : "text/html",
+        'Access-Control-Allow-Origin': 'http://13.124.86.217'
+    });
+    response.write('<html>\n<head>\n<title>Simple \n</title>\n</head>\n');
+    response.write('<body>\n');
     for( var idx in messages){
-        res.write('\n<h1>' + messages[idx] + '</h1>');
+        response.write('\n<h1>' + messages[idx] + '</h1>');
     }
-    res.end('\n</body>\n</html>');
+    response.end('\n</body>\n</html>');
 }).listen(8081);
 
--- Chapter7/http_server_static.js
+++ Chapter7/http_server_static.js
@@ -9,15 +9,26 @@ var ROOT_DIR = "html/";
 
 var callbackServer = function(request, response){
     var urlObj = url.parse( request.url, true, false );     // url을 파싱
+
     fs.readFile(ROOT_DIR + urlObj.pathname, function(err, data){
         if( err ){
             console.log( JSON.stringify(err));
-            response.writeHead(404);
+            response.writeHead(404, {
+                'Access-Control-Allow-Origin': 'http://13.124.86.217'
+            });
             response.end(JSON.stringify(err) + " ==> cannot find file !! : " + urlObj.pathname );
-            return;
         }
         else {
-            response.writeHead(200);
+            var fsState = fs.statSync(ROOT_DIR + urlObj.pathname);
+            var stringSize = fsState.size;
+            console.log( "file Size : " + stringSize );
+
+            response.writeHead(200, {
+                'Content-Length': stringSize,
+                 'Content-Type' : 'text/html',
+                  'Access-Control-Allow-Origin': 'http://13.124.86.217'
+            });
+
             response.end(data);
             console.log("Success!! " + (ROOT_DIR + urlObj.pathname) + " is sending OK!!" );
         }
