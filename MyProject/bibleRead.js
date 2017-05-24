bf2fb951507e0c7999651ecb278cf582689baa53
David Lee <karisma70kr@gmail.com>
2017-05-24 11:52:54 +0900

    성경 텍스트 파일을 읽어서 mongodb에 입력하는 소스 1차 완료

--- MyProject/bibleRead.js
+++ MyProject/bibleRead.js
@@ -2,11 +2,6 @@
  * Created by Administrator on 2017-05-22.
  */
 
-var chapterTitle = require('./chapterTitle');
-var fs = require('fs');
-
-var options = { encoding: 'utf8', flag: 'r' };
-
 function isNumber( str ) {
     str += ''; // 문자열로 변환
     str = str.replace(/^\s*|\s*$/g, ''); // 좌우 공백 제거
@@ -15,91 +10,105 @@ function isNumber( str ) {
     return true;
 }
 
-function getRowContent( rowString ){
-    chapterPos = rowString.indexOf(" ");
+var readBibleText = (function(){
 
-    if( chapterPos < 1 )
-        return;
+    var chapterTitle = require('./chapterTitle');
+    var fs = require('fs');
 
-    var chapterText = rowString.substring( 0, chapterPos );
-    var numberLoc = chapterText.indexOf( ":");
+    var options = { encoding: 'utf8', flag: 'r' };
 
-    var chapterNum = "";
-    var shortTitle = "";
+    var bibleContents = new Array();
 
-    for (var i = 0; i < numberLoc; i++) {
-        var text = chapterText.charAt(i);
-        if (isNumber(text))
-            chapterNum += text;
-        else
-            shortTitle += text;
-    }
+    function getRowContent( rowString ){
+        var chapterPos = rowString.indexOf(" ");
 
-    chapterNum = parseInt(chapterNum);
-    chapterNum = chapterNum.toString();
-
-    var paragraphNum = rowString.substring(numberLoc + 1, chapterText.length);
-    paragraphNum = parseInt(paragraphNum);
-    paragraphNum = paragraphNum.toString();
-
-    var content = rowString.substring(chapterText.length+1, rowString.length);
-
-    var chapterObj = chapterTitle.findByChar( shortTitle );
-
-    if( chapterObj ) {
-        var bibleRowText = {
-            "shortTitle": shortTitle,
-            "title": chapterObj.name,
-            "chapter": chapterNum,
-            "paragraph": paragraphNum,
-            "content": content
-        };
-
-        return bibleRowText;
-    }
-    else{
-        console.log( shortTitle + " is Error!!" );
-        return null;
-    }
-}
-
-var bibleContents = new Array();
-// var bibleContents = [];
-
-fs.readFile( 'data/hangul_bible_utf8.txt', options, function(err, data){
-    if( err ) {
-        console.log("Failed to open bible file!! " + err);
-        return;
-    }
-
-    var bibleTextArray = data.split("\n");
-    var rowCount = bibleTextArray.length;
-    var insCount = 0;
-
-    for( var row = 0; row < rowCount ; row ++ ){
-        var rowString = bibleTextArray[row];
-        var rowObj = getRowContent( rowString );
-        if( rowObj ) {
-            bibleContents.push(rowObj);
-            insCount ++;
-        }
-    }   //  end of for
-
-    console.log( "total bible inserted row : " + insCount );
-
-    writeFile();
-
-});
-
-// console.log( "array Count : " + bibleContents.length );
-
-function writeFile() {
-    fs.writeFile('data/hangul_bible_utf8.json', JSON.stringify(bibleContents), function (err) {
-        if (err) {
-            console.log(err);
+        if( chapterPos < 1 )
             return;
+
+        var chapterText = rowString.substring( 0, chapterPos );
+        var numberLoc = chapterText.indexOf( ":");
+
+        var chapterNum = "";
+        var shortTitle = "";
+
+        for (var i = 0; i < numberLoc; i++) {
+            var text = chapterText.charAt(i);
+            if (isNumber(text))
+                chapterNum += text;
+            else
+                shortTitle += text;
         }
 
-        console.log("JSON File write complete!!! ");
-    });
-}
+        chapterNum = parseInt(chapterNum);
+        chapterNum = chapterNum.toString();
+
+        var paragraphNum = rowString.substring(numberLoc + 1, chapterText.length);
+        paragraphNum = parseInt(paragraphNum);
+        paragraphNum = paragraphNum.toString();
+
+        var content = rowString.substring(chapterText.length+1, rowString.length);
+
+        var chapterObj = chapterTitle.findByChar( shortTitle );
+
+        if( chapterObj ) {
+            var bibleRowText = {
+                "shortTitle": shortTitle,
+                "title": chapterObj.name,
+                "chapter": chapterNum,
+                "paragraph": paragraphNum,
+                "content": content
+            };
+
+            return bibleRowText;
+        }
+        else{
+            console.log( shortTitle + " is Error!!" );
+            return null;
+        }
+    }
+
+    function writeJSONFile() {
+        fs.writeFile('data/hangul_bible_utf8.json', JSON.stringify(bibleContents), function (err) {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            console.log("JSON File write complete!!! ");
+        });
+    }
+
+    return function( fileName, callback ) {
+        // fs.readFile('data/hangul_bible_utf8.txt', options, function (err, data) {
+        fs.readFile( fileName, options, function (err, data) {
+            if (err) {
+                console.log("Failed to open bible file!! " + err);
+                return;
+            }
+
+            var bibleTextArray = data.split("\n");
+            var rowCount = bibleTextArray.length;
+            console.log( "whole row count : " + rowCount );
+
+            for (var row = 0; row < rowCount; row++) {
+                var rowString = bibleTextArray[row];
+                var rowObj = getRowContent(rowString);
+                if( rowObj ) {
+                    callback(rowObj);
+                }
+            }   //  end of for
+
+        });
+    }
+
+}( ));
+
+/*
+readBibleText( function( rowObj ){
+    console.log( rowObj );
+});
+    */
+
+
+module.exports = {
+    readBibleText : readBibleText
+};
--- MyProject/insertBible.js
+++ MyProject/insertBible.js
@@ -3,63 +3,7 @@
  */
 
 var MongoClient = require('mongodb').MongoClient;
-var chapterTitle = require('./chapterTitle');
-var fs = require('fs');
-
-function isNumber( str ) {
-    str += ''; // 문자열로 변환
-    str = str.replace(/^\s*|\s*$/g, ''); // 좌우 공백 제거
-    if (str == '' || isNaN(str))
-        return false;
-    return true;
-}
-
-function getRowContent( rowString ){
-    chapterPos = rowString.indexOf(" ");
-
-    if( chapterPos < 1 )
-        return;
-
-    var chapterText = rowString.substring( 0, chapterPos );
-    var numberLoc = chapterText.indexOf( ":");
-
-    var chapterNum = "";
-    var shortTitle = "";
-
-    for (var i = 0; i < numberLoc; i++) {
-        var text = chapterText.charAt(i);
-        if (isNumber(text))
-            chapterNum += text;
-        else
-            shortTitle += text;
-    }
-
-    chapterNum = parseInt(chapterNum);
-    chapterNum = chapterNum.toString();
-
-    var paragraphNum = rowString.substring(numberLoc + 1, chapterText.length);
-    paragraphNum = parseInt(paragraphNum);
-    paragraphNum = paragraphNum.toString();
-
-    var content = rowString.substring(chapterText.length+1, rowString.length);
-
-    var chapterObj = chapterTitle.findByChar( shortTitle );
-
-    if( chapterObj ) {
-        return {
-            shortTitle: shortTitle,
-            title: chapterObj.name,
-            chapter: chapterNum,
-            paragraph: paragraphNum,
-            content: content
-        };
-    }
-    else{
-        console.log( shortTitle + " is Error!!" );
-        return null;
-    }
-}
-
+var bibleRead = require('./bibleRead');
 
 function addObject( collection, recordObj ){
     collection.insert( recordObj, function(err, result ){
@@ -67,8 +11,6 @@ function addObject( collection, recordObj ){
             console.log("collection.insert Error!!!  : " + err );
             return false;
         }
-
-//        console.log("Inserted : " + result );
     });
 }
 
@@ -81,31 +23,13 @@ function InsertRecordsToCollection(err, collection ){
         return;
     }
 
-    fs.readFile( 'data/hangul_bible_utf8.txt', { encoding: 'utf8', flag: 'r' },  function( ferr, data) {
-        if( ferr ) {
-            console.log("Failed to open bible file!! " + ferr);
-            return;
+    bibleRead.readBibleText( 'data/hangul_bible_utf8.txt', function( rowObj ){
+        if( rowObj ){
+            addObject( collection, rowObj );
+            insCount ++;
         }
-
-        var bibleTextArray = data.split("\n");
-        var rowCount = bibleTextArray.length;
-
-        console.log("total bible row Count : " + rowCount );
-
-        for( var row = 0; row < rowCount ; row ++ ) {
-            var rowString = bibleTextArray[row];
-            var rowObj = getRowContent(rowString);
-
-            if( rowObj ) {
-                addObject( collection, rowObj );
-                insCount ++;
-            }
-        }
-
-    } );
-
+    });
 }
-
 
 MongoClient.connect( "mongodb://bibleAdmin:daejin70@localhost:1001/bible_service", function(err, db){
 
@@ -123,7 +47,10 @@ MongoClient.connect( "mongodb://bibleAdmin:daejin70@localhost:1001/bible_service
         myDB.dropCollection("bible", function(err, result ){
             if( err ){
                 console.log( err );
+            }else{
+                console.log( "drop result : " + result );
             }
+
         });
 
         myDB.createCollection("bible",  InsertRecordsToCollection );
@@ -131,6 +58,6 @@ MongoClient.connect( "mongodb://bibleAdmin:daejin70@localhost:1001/bible_service
         setTimeout( function(){
             db.close();
             console.log(" db.close() insert Count : " + insCount );
-        }, 3000 );
+        }, 10000 );
     }
 } );
