6e19f2a3265af16efe910a139ee199d6ff61a5b4
David Lee <karisma70kr@gmail.com>
2017-05-25 10:23:09 +0900

    성경검색 프로토타입 코드

---
+++ MyProject/searchBible.js
@@ -0,0 +1,39 @@
+/**
+ * Created by Administrator on 2017-05-24.
+ */
+
+var  searchBible = (function() {
+    var MongoClient = require('mongodb').MongoClient;
+
+    MongoClient.connect("mongodb://bibleAdmin:daejin70@localhost:1001/bible_service", function (err, db) {
+
+        if (err) {
+            console.log("Fail!!! Connect to mongodb : " + err);
+            return;
+        }
+
+        console.log("Success authenticate!! ");
+
+        var myDB = db.db("bible_service");
+        if (myDB) {
+            console.log("db is exist");
+        }
+
+        myDB.collection("bible", function (err, bible) {
+            bible.find({title: "열왕기상", chapter: "5", paragraph: "1"}, function (err, items) {
+                console.log("----------------------------------");
+                items.toArray(function (err, itemArr) {
+                    console.log("Document Array : ");
+                    console.log(itemArr);
+                });
+            });   // end of nebulae.find
+        });
+
+        setTimeout(function () {
+            db.close();
+            console.log("db.close() ");
+        }, 3000);
+
+    });
+
+}());
