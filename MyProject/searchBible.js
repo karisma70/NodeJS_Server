3d922494568b1fe872ae0c8ece819e0306e6d6c9
David Lee <karisma70kr@gmail.com>
2017-05-26 16:21:51 +0900

    성경의 book, chapter, paragraph 검색 완료

--- MyProject/searchBible.js
+++ MyProject/searchBible.js
@@ -2,18 +2,12 @@
  * Created by Administrator on 2017-05-24.
  */
 
-var  searchBible = (function() {
-    var MongoClient = require('mongodb').MongoClient;
+var http = require('http');
+var url = require('url');
 
-    function callbackSearch(err, bible) {
-        bible.find({title: "열왕기상", chapter: "5", paragraph: "1"}, function (err, items) {
-            console.log("----------------------------------");
-            items.toArray(function (err, itemArr) {
-                console.log("Document Array : ");
-                console.log(itemArr);
-            });
-        });   // end of nebulae.find
-    }
+var  searchBible = (function() {
+    var bibleDB = null;
+    var MongoClient = require('mongodb').MongoClient;
 
     function callbackDB( err, db ){
         if (err) {
@@ -23,19 +17,106 @@ var  searchBible = (function() {
 
         console.log("Success authenticate!! ");
 
-        var myDB = db.db("bible_service");
-        if (myDB) {
-            console.log("db is exist");
+        bibleDB = db.db("bible_service");
+        if ( bibleDB) {
+            console.log("bibleDB is ready!!");
         }
-
-        myDB.collection("bible", callbackSearch );
-
-        setTimeout(function () {
-            db.close();
-            console.log("db.close() ");
-        }, 3000);
     }
 
-    MongoClient.connect("mongodb://bibleAdmin:daejin70@localhost:1001/bible_service", callbackDB );
+    function Search( option, callback ) {
+        if( bibleDB == null ){
+            console.log( "bibleDB is not valid!!");
+            return;
+        }
+        bibleDB.collection("bible", function(err, bible) {
+            bible.find( option, callback );
+        } );
+    }
+
+    return {
+        connect : function(){
+            MongoClient.connect("mongodb://bibleAdmin:daejin70@localhost:1001/bible_service", callbackDB );
+        },
+        search : Search,
+        close : function(){
+            setTimeout(function () {
+                bibleDB.close();
+                console.log("db.close() ");
+            }, 3000);
+        }
+    }
 
 }());
+
+searchBible.connect();
+
+
+function sendNoResponse( response ){
+    response.writeHead(200, {
+        'Content-Length': "100",
+        'Content-Type' : 'text/html',
+        'Access-Control-Allow-Origin': 'http://13.124.86.217'
+    });
+    response.end( null );
+}
+
+function searchAndSendData( searchOption, response) {
+
+    // searchBible.search({title: "창세기", chapter: "1", paragraph: "1"}, function (err, items) {
+    searchBible.search( searchOption, function (err, items) {
+        if (err) {
+            console.log("recored fetch Error!!! " + err);
+            console.log(JSON.stringify(err));
+            response.writeHead(404, {
+                'Access-Control-Allow-Origin': 'http://13.124.86.217'
+            });
+            response.end(JSON.stringify(err));
+            return;
+        }
+
+        items.toArray( function(err, itemArr){
+            if( err ){
+                console.log( "items.toArray : " + err );
+                return;
+            }
+            console.log("Document Array : ");
+            console.log( JSON.stringify(itemArr));
+
+            response.writeHead(200, {
+                // 'Content-Length': content.length,
+                'Content-Type': 'text/html',
+                'Access-Control-Allow-Origin': 'http://13.124.86.217'
+            });
+
+            response.end(JSON.stringify( itemArr ));
+        });
+
+    });
+}
+
+
+var callbackServer = function(request, response){
+    var urlObj = url.parse( request.url, true, false );     // url을 파싱
+    var reqData = '';
+
+    if( request.method == "HEAD"){
+        sendNoResponse( response );
+        return;
+    } else if(request.method == "POST"){
+        request.on('data', function(chunk){
+            reqData += chunk;
+        });
+        request.on('end', function(){
+            console.log( reqData );
+            var searchOpt = JSON.parse( reqData );
+            searchAndSendData( searchOpt, response);
+        });
+    } else {
+        console.log( "url : " + urlObj.pathname );
+        sendNoResponse( response );
+        return;
+    }
+};
+
+
+http.createServer( callbackServer ).listen( 8082 );
